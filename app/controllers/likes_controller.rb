8b98067c6d2a73c020f5738074f573ba10023b91
kamaya <r.k.y.g.7.0813@gmail.com>
2017-08-10 23:48:38 +0900

    initial commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,5 +2,6 @@ class LikesController < ApplicationController
 	def create
 		@like = Like.create(post_id: params[:post_id])
 		@likes = Like.where(post_id: params[:post_id])
+		redirect_to posts_path
 	end
 end
